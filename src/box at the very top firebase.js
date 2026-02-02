a7cac686c4195c68f06b8846bfc2be254f3ea3ad
JJLuv32 <jjluv2009@hotmail.com>
2026-02-01 21:55:58 -0500

    Add Firebase initialization in firebase.js

---
+++ src/box at the very top firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVPZLqm_qxqof9q0O3v1uJeP-eIwSkGyc",
+  authDomain: "big-game-boxes.firebaseapp.com",
+  databaseURL: "https://big-game-boxes-default-rtdb.firebaseio.com",
+  projectId: "big-game-boxes",
+  storageBucket: "big-game-boxes.firebasestorage.app",
+  messagingSenderId: "577628785747",
+  appId: "1:577628785747:web:daf564a3f733a683700aa6",
+  measurementId: "G-Z0B6BHC4J7"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
